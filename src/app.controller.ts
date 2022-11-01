f6f799ee1220b748798073fc3fe848172adbb6b1
t-montes <t.montes@uniandes.edu.co>
2022-10-31 22:11:54 -0500

    RANDOM - add dummy endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Get, Request, UseGuards } from '@nestjs/common';
+import { Controller, Post, Get, Request, UseGuards, Res, Param } from '@nestjs/common';
+import { Response } from 'express';
 import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { AppService } from './app.service';
@@ -36,5 +37,11 @@ export class AppController {
   @Post('auth/login')
   async authlogin(@Request() req) {
       return this.authService.login(req.user);
+  }
+
+  @Get('dummy/:code')
+  async dummy(@Param('code') code: string, @Res({ passthrough:true }) res: Response) {
+    res.status((isNaN(+code) || +code < 200 || +code > 999) ? 418 : +code);
+    return { code };
   }
 }
