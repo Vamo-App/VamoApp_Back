798a4920671e8af03cb6295b3b74cc2db55398c7
Braito <b.riverah@uniandes.edu.co>
2022-11-21 14:11:44 -0500

    already finished VAMO!

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,7 +8,7 @@ import { Business } from "./business/business.entity";
 import { CredentialsDto } from "./shared/utils/credentials";
 import { Place } from "./place/place.entity";
 import { Weight } from "./weight/weight.entity";
-import { distance } from './shared/utils/functions';
+import { distance } from "./shared/utils/functions";
 @Injectable()
 export class AppService {
   constructor(
@@ -43,7 +43,6 @@ export class AppService {
     longitude: number,
     latitude: number,
     radius: number
-    
   ): Promise<Place[]> {
     let client: Client[] = [];
     let HashTable = new Map();
@@ -65,40 +64,55 @@ export class AppService {
         let peso = Weight[j].weight;
         if (HashTable.has(tag)) {
           let value = HashTable.get(tag);
-          HashTable.set(tag, value + peso/client.length);
+          HashTable.set(tag, value + peso / client.length);
         } else {
-          HashTable.set(tag, peso/client.length);
+          HashTable.set(tag, peso / client.length);
         }
       }
     }
-    best_tags = Array.from(HashTable.entries()).sort((a, b) => b[1] - a[1]);
+    best_tags = Array.from(HashTable.entries());
     let places_in_order = [];
-    let ids:String[] = [];
+    let ids: String[] = [];
     for (let i = 0; i < best_tags.length; i++) {
       let tag = best_tags[i][0];
       let places = await this.placeRepository
         .createQueryBuilder("place")
         .leftJoinAndSelect("place.tags", "tags")
-        .where(':tag IN (SELECT ptp."tagTag" FROM tag_places_place ptp where ptp."placeId"=place.id)', {tag: tag})
+        .where(
+          ':tag IN (SELECT ptp."tagTag" FROM tag_places_place ptp where ptp."placeId"=place.id)',
+          { tag: tag }
+        )
         .getMany();
-      places = places.filter( (place) => {
-        let distance_ = distance(place.latitude, place.longitude, latitude, longitude);
-        if (distance_ <= radius) {
-          console.log(place.name)
-          console.log(distance_)
-        }
-        return distance_ <= radius
+      places = places.filter((place) => {
+        let distance_ = distance(
+          place.latitude,
+          place.longitude,
+          latitude,
+          longitude
+        );
+        return distance_ <= radius;
       });
+      //TODO aquí se tiene que calcular el puntaje de cada lugar
+      // y ver qué tanta favorabilidad tiene este según sus etiquetas con las etiquetas favorables del cliente
+      // y ordenarlos de mayor a menor
       for (let j = 0; j < places.length; j++) {
-        let place = places[j];
-        if ( ids.indexOf(place.id) == -1) {
-          places_in_order.push(place)
-          ids.push(place.id);
+        let places_tags=places[j].tags;
+        let  puntaje =0;
+        if (ids.includes(places[j].id)) {
+          continue;
         }
+        for (let k = 0; k < places_tags.length; k++) {
+          let place_tag=places_tags[k].tag;
+          if (HashTable.has(place_tag)) {
+            puntaje+=HashTable.get(place_tag);
+          }
+        }
+        places_in_order.push([places[j],puntaje]);
+        ids.push(places[j].id);
       }
+      
     }
-
-    return places_in_order;
+    return places_in_order.sort((a,b)=>b[1]-a[1]).map((a)=>a[0]);
   }
 
   async loginBusiness(credentials: CredentialsDto): Promise<Business> {
